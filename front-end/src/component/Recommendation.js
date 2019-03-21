6ab584c4d818eaf873090e634257233cf376b44a
Koeun Lee <koeunhh@gmail.com>
2019-03-21 01:02:43 -0400

    play recommended song

--- front-end/src/component/Recommendation.js
+++ front-end/src/component/Recommendation.js
@@ -4,22 +4,50 @@ export default class Recommendation extends Component {
   constructor(props){
     super(props);
     this.state = {
-      play: false
+      play: false,
+      index: 0
     }
   }
 
-  togglePlay = () => {
+  togglePlay = (url) => {
+    this.setState({play: !this.state.play})
+  }
 
+  nextSong = () => {
+    if(this.state.index < 5){
+      this.setState({index: (this.state.index + 1)});
+    }
+  }
+
+  previousSong = () => {
+    if(this.state.index > 0){
+      this.setState({index: (this.state.index - 1)});
+    }
   }
 
   render(){
     const {topTracks} = this.props;
-    const preview_url = topTracks[0].preview_url;
+    const currentTrack = topTracks[this.state.index];
+
+    const albumCover = currentTrack.album.images[2].url;
+    const preview_url = currentTrack.preview_url;
+
     const audio = new Audio(preview_url);
+    if(this.state.play === true){
+      // console.log(this.state.play);
+      audio.play()
+    }
+    else{
+      // console.log(this.state.play);
+      audio.pause()
+    }
 
     return(
       <div className='recommendation'>
-        {/* <button onClick={audio.play}>Play</button> */}
+        <img onClick={this.previousSong} src='./assets/arrow-previous.svg' alt='previous'/>
+        <img src = {albumCover} alt='albumCover'/>
+        <button onClick={this.togglePlay}>Play</button>
+        <img onClick={this.nextSong} src='./assets/arrow-next.svg' alt='next'/>
       </div>
     )
   }
